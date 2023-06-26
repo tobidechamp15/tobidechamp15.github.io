93245a8991b96dc7288cd9a1d348d1210219404f
unknown <tobidechamp15@gmail.com>
2023-06-26 14:25:22 +0100

    form-validated

--- main.js
+++ main.js
@@ -1,34 +1,91 @@
-const forms = document.getElementById("des-form");
- const error= document.querySelector(".error")
-const fname = document.getElementById("fname");
-const sname = document.getElementById("sname");
-const email = document.getElementById("email");
-const serror = document.getElementById("snameError");
+const firstName = document.getElementById("firstName");
+const surName = document.getElementById("surName");
+const email = document.getElementById("Email");
+const comment = document.getElementById("comment");
+const nameError = document.getElementById("nameError");
+const surNameError = document.getElementById("surNameError");
+const emailError = document.getElementById("emailError");
+const commentError = document.getElementById("commentError");
 
-function validateInput() {
-  if (!fname.value) {
-     error.classList.remove("hidden")
+function validateForm(e) {
+  e.preventDefault();
+
+  if (!firstName.value) {
+    nameError.classList.add("visible");
+    firstName.setAttribute("aria-invalid", true);
+    firstName.setCustomValidity("First name is required");
   } else {
-    error.classList.add("hidden")
-   }
-    
-  if (!sname.value) {
-    error.classList.remove("hidden");
+    nameError.classList.remove("visible");
+    firstName.setAttribute("aria-invalid", false);
+    firstName.setCustomValidity("");
+  }
+
+  if (!surName.value) {
+    surNameError.classList.add("visible");
+    surName.setAttribute("aria-invalid", true);
+    surName.setCustomValidity("Surname is required");
   } else {
-    error.classList.add("hidden");
-     console.log(fname.value);
+    surNameError.classList.remove("visible");
+    surName.setAttribute("aria-invalid", false);
+    surName.setCustomValidity("");
+  }
+
+  if (!email.value) {
+    emailError.classList.add("visible");
+    email.setAttribute("aria-invalid", true);
+    email.setCustomValidity("Email is required");
+  } else if (!isValidEmail(email.value)) {
+    emailError.classList.add("visible");
+    email.setAttribute("aria-invalid", true);
+    email.setCustomValidity("Invalid email format");
+  } else {
+    emailError.classList.remove("visible");
+    email.setAttribute("aria-invalid", false);
+    email.setCustomValidity("");
+  }
+
+  if (!comment.value) {
+    commentError.classList.add("visible");
+    comment.setAttribute("aria-invalid", true);
+    comment.setCustomValidity("Comment is required");
+  } else {
+    commentError.classList.remove("visible");
+    comment.setAttribute("aria-invalid", false);
+    comment.setCustomValidity("");
+  }
+
+  // If there are no errors, submit the form
+  if (
+    firstName.checkValidity() &&
+    surName.checkValidity() &&
+    email.checkValidity() &&
+    comment.checkValidity()
+  ) {
+    // Form is valid, submit it
+    document.getElementById("myForm").submit();
   }
 }
-function validate(event){
-  event.preventDefault();
-  validateInput();
+
+function isValidEmail(email) {
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
-
-forms.addEventListener("submit", validate);
-console.log(forms);
-
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", validateForm);
 
 
-console.log(fname);
 
+
+const navBar = document.querySelector("#righthead");
+
+navBar.addEventListener("click", clickMe);
+const navList = document.querySelector("#overlay");
+
+function clickMe() {
+  navList.style.display=("block")
+  console.log((2+3).value);
+}
+console.log(navList)
+
+console.log(navBar);
